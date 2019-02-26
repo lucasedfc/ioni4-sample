d03b2093480937fe44e2ffcdb6d5963555c0f2d5
Lucas Heim <lheim@asesconsulting.com>
2019-02-26 13:32:42 -0300

    types of buttons

--- 01-components/src/app/pages/inicio/inicio.page.ts
+++ 01-components/src/app/pages/inicio/inicio.page.ts
@@ -22,6 +22,11 @@ export class InicioPage implements OnInit {
       icon: 'beaker',
       name: 'Avatar',
       redirectTo: '/avatar'
+    },
+    {
+      icon: 'radio-button-on',
+      name: 'Buttons - Router',
+      redirectTo: '/buttons'
     }
 
   ];
