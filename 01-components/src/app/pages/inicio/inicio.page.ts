340a49e6b8a8d5b141753883e05a30d5cdd4c78b
Lucas Heim <lheim@asesconsulting.com>
2019-02-26 13:57:35 -0300

    ion-card example

--- 01-components/src/app/pages/inicio/inicio.page.ts
+++ 01-components/src/app/pages/inicio/inicio.page.ts
@@ -27,6 +27,11 @@ export class InicioPage implements OnInit {
       icon: 'radio-button-on',
       name: 'Buttons - Router',
       redirectTo: '/buttons'
+    },
+    {
+      icon: 'card',
+      name: 'Cards',
+      redirectTo: '/card'
     }
 
   ];
