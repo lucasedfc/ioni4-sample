856d1d075a136ece9ff32a6f4ba8348deb228b98
Lucas Heim <lheim@asesconsulting.com>
2019-02-26 11:41:57 -0300

    Alert controller example

--- 01-components/src/app/pages/alert/alert.page.ts
+++ 01-components/src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -7,9 +8,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  title: string;
+
+  constructor(private alertCtrl: AlertController) {
+    this.title = "Alert Page"
+   }
 
   ngOnInit() {
+  }
+
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'OK',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async presentAlertPrompt() {
+    const alert = await this.alertCtrl.create({
+      header: 'What is your name?',
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          placeholder: 'Please type your name'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: (e) => {
+            console.log('Confirm Ok', e);
+            this.title = e.name;
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
