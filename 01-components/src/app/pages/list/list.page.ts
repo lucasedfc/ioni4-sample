ee671be9e87e3bcb7d7c3e4f6ff809802e161846
Lucas Heim <lheim@asesconsulting.com>
2019-02-27 14:55:40 -0300

    ion list sliding

--- 01-components/src/app/pages/list/list.page.ts
+++ 01-components/src/app/pages/list/list.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
 import { Observable } from 'rxjs';
+import { IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -9,6 +10,7 @@ import { Observable } from 'rxjs';
 })
 export class ListPage implements OnInit {
 
+  @ViewChild('list') listUser: IonList; 
   users:Observable<any>;
 
   constructor(
@@ -18,5 +20,21 @@ export class ListPage implements OnInit {
   ngOnInit() {
     this.users = this.dataService.getUsers();
   }
+
+  favorite(user: any){
+    console.log('favorite', user);
+    this.listUser.closeSlidingItems();
+  }
+
+  share(user: any){
+    console.log('share', user);
+    this.listUser.closeSlidingItems();
+  }
+
+  delete(user: any){
+    console.log('delete', user);
+    this.listUser.closeSlidingItems();
+  }
+
 
 }
