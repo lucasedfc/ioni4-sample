e14ece4ee9e5ab740027453793d59c9a79dcc674
Lucas Heim <lheim@asesconsulting.com>
2019-02-27 12:03:50 -0300

    input form

---
+++ 01-components/src/app/pages/input/input.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.page.html',
+  styleUrls: ['./input.page.scss'],
+})
+export class InputPage implements OnInit {
+
+  name: string;
+  user = {
+    email: '',
+    password: ''
+  }
+
+  constructor() {
+    this.name = '';
+   }
+
+  ngOnInit() {
+  }
+
+  onSubmitTemplate(){
+    console.log("Form submited");
+    console.log("User data", this.user);
+  }
+
+}
